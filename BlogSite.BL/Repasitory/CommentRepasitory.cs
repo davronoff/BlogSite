3514910fe6933c242fa69236275a0d7ea9631caa
davronoff <ilyosbekdavronov009@gmail.com>
2022-04-06 14:32:46 +0500

    write repasitory

--- BlogSite.BL/Repasitory/CommentRepasitory.cs
+++ BlogSite.BL/Repasitory/CommentRepasitory.cs
@@ -1,5 +1,7 @@
 ﻿using BlogSite.BL.Interface;
+using BlogSite.Data.DataLayer;
 using BlogSite.Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,29 +12,42 @@ namespace BlogSite.BL.Repasitory
 {
     public class CommentRepasitory : ICommentInterface
     {
+        private readonly AppDbContext _commentdb;
+
+        public CommentRepasitory(AppDbContext commentdb)
+        {
+            _commentdb = commentdb;
+        }
+
+        //Write comment
         public Task<Comment> AddComment(Comment newComment)
         {
-            throw new NotImplementedException();
+            _commentdb.comments.Add(newComment);
+            _commentdb.SaveChanges();
+            return Task.FromResult(newComment);
         }
 
+        //Delete comment, this method delete from database
         public Task DeleteComment(Guid id)
         {
-            throw new NotImplementedException();
+            Comment comment = _commentdb.comments.FirstOrDefault(p => id == p.Id);
+            _commentdb.comments.Remove(comment);
+            _commentdb.SaveChanges();
+            return Task.FromResult(0);
         }
 
-        public Task<Comment> GetComment(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+        //Get comment only one by CommentID
+        public Task<Comment> GetComment(Guid id) => _commentdb.comments.FirstOrDefaultAsync(p => id == p.Id);
 
-        public Task<List<Comment>> GetComments()
-        {
-            throw new NotImplementedException();
-        }
+        //Get comments for all written comments
+        public Task<List<Comment>> GetComments() => _commentdb.comments.ToListAsync();
 
+        //Change comment 
         public Task<Comment> UpdateComment(Comment comment)
         {
-            throw new NotImplementedException();
+            _commentdb.comments.Update(comment);
+            _commentdb.SaveChanges();
+            return Task.FromResult(comment);
         }
     }
 }
--- BlogSite.BL/Repasitory/PostRepasitory.cs
+++ BlogSite.BL/Repasitory/PostRepasitory.cs
@@ -1,5 +1,7 @@
 ﻿using BlogSite.BL.Interface;
+using BlogSite.Data.DataLayer;
 using BlogSite.Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,29 +12,42 @@ namespace BlogSite.BL.Repasitory
 {
     public class PostRepasitory : IPostInterface
     {
+        private readonly AppDbContext _postdb;
+
+        public PostRepasitory(AppDbContext postdb)
+        {
+            _postdb = postdb;
+        }
+
+        //Add post fro site page
         public Task<Post> AddPost(Post newPost)
         {
-            throw new NotImplementedException();
+            _postdb.posts.Add(newPost);
+            _postdb.SaveChanges();
+            return Task.FromResult(newPost);
         }
 
+        //Delete post, this method delete from database
         public Task DeletePost(Guid id)
         {
-            throw new NotImplementedException();
+            Post post =  _postdb.posts.FirstOrDefault(p => id == p.Id);
+            _postdb.posts.Remove(post);
+            _postdb.SaveChanges();
+            return Task.FromResult(0);
         }
 
-        public Task<Post> GetPost(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+        //Get post only one post by PostId
+        public Task<Post> GetPost(Guid id) => _postdb.posts.FirstOrDefaultAsync(p => id == p.Id);
 
-        public Task<List<Post>> GetPosts()
-        {
-            throw new NotImplementedException();
-        }
+        //Get posts for add all post 
+        public Task<List<Post>> GetPosts() => _postdb.posts.ToListAsync();
 
+        //Change post
         public Task<Post> UpdatePost(Post post)
         {
-            throw new NotImplementedException();
+            _postdb.posts.Update(post);
+            _postdb.SaveChanges();
+            return Task.FromResult(post);
         }
     }
 }
